4dac703f7fdb12cda2aa6f05d1f67fd9047275e9
Chizaram Opara <chineduelusanmi@gmail.com>
2022-07-13 12:05:49 +0100

    Updates

---
+++ precache-manifest.b0941e0c2fbf71ab20e2f87fbead0244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc0cb4f3e0b1e23ad3b7",
+    "url": "/Superhero/static/css/main.0cd04905.chunk.css"
+  },
+  {
+    "revision": "dc0cb4f3e0b1e23ad3b7",
+    "url": "/Superhero/static/js/main.dc0cb4f3.chunk.js"
+  },
+  {
+    "revision": "b2eecc91562cabb45d43",
+    "url": "/Superhero/static/js/1.b2eecc91.chunk.js"
+  },
+  {
+    "revision": "c121dccf76c39bac2b83",
+    "url": "/Superhero/static/js/runtime~main.c121dccf.js"
+  },
+  {
+    "revision": "d9fbf8e3961669d75167ddc693663232",
+    "url": "/Superhero/index.html"
+  }
+];
